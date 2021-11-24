9f31be1162588ea6ec95f04f026666d5b5ac27e2
Stanley <yelnalts1993@gmail.com>
2021-11-24 11:46:52 -0300

    Mapa renderizado

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { LatLngExpression } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import  './MapComponent.css'
 
 function MapComponent() {
     
-  return (
-   <></>
-  );
+ return (
+
+ 
+  <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[51.505, -0.09]}>
+    <Popup>
+      A pretty CSS3 popup. <br /> Easily customizable.
+    </Popup>
+  </Marker>
+</MapContainer>
+ )};
+
+function createLngLat(lon:number, lat:number): LatLngExpression{        
+    return {lng: lon, lat: lat}
 }
 
 export default MapComponent;
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import NavBarComponent from '../components/Navbar/NavBarComponent';
-
+import MapComponent from '../components/Map/MapComponent';
 function Index() {
   return (
+
     <div className="Index">
+      <link
+     href="<https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800&display=swap>"
+     rel="stylesheet"
+/>
         <NavBarComponent />
-     
+        
+        <MapComponent />
+        
     </div>
   );
 }
