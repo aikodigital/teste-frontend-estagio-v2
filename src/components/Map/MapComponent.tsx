8c7e02d64a90b45c60eb80d7b5e298e95e3e7812
Stanley <yelnalts1993@gmail.com>
2021-11-24 23:21:49 -0300

    historico dos equipamentos(sem css concluido)

--- src/components/Map/MapComponent.tsx
+++ src/components/Map/MapComponent.tsx
@@ -1,4 +1,4 @@
-import { LatLngExpression, popup } from 'leaflet';
+import { LatLngExpression } from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import './MapComponent.css';
 import locData from '../../data/equipmentPositionHistory.json';
@@ -10,21 +10,39 @@ import { render } from '@testing-library/react';
 
 function MapComponent() {
 
-  
-   
 
-  const LeafletMarkers = locData.map(marker => (
-    <Marker position={[marker.positions[marker.positions.length - 1].lat, marker.positions[marker.positions.length - 1].lon]} key={`marker_${marker.equipmentId}`}>
-      
-      <Popup>
-        <span> Equipamento : {equipmentName(marker.equipmentId)} <br></br></span>
-        <span> Modelo do equipamento : {equipmentModelId(marker.equipmentId)}  <br></br></span>
-        <span> Id do equipamento : {marker.equipmentId} <br></br></span>
-        <span> Estado do equipamento : {equipmentState(marker.equipmentId)} <br></br></span>
-        <span> Histórico de Estados do equipamento : <br></br></span>
-      </Popup>
-    </Marker>
-  ));
+
+
+  const LeafletMarkers = locData.map(marker => {
+
+    marker.positions = marker.positions.sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime()))
+
+
+
+
+
+    return (
+      <Marker position={[marker.positions[0].lat, marker.positions[0].lon]} key={`marker_${marker.equipmentId}`}>
+
+        <Popup className="pop">
+          <span> Equipamento : {equipmentName(marker.equipmentId)} <br></br></span>
+          <span> Modelo do equipamento : {equipmentModelId(marker.equipmentId)}  <br></br></span>
+          <span> Id do equipamento : {marker.equipmentId} <br></br></span>
+          <span> Estado do equipamento : {equipmentState(marker.equipmentId)} <br></br></span>
+          <span> Histórico de Estados do equipamento : <br></br></span>
+          <button onClick={() => showHistory(marker.equipmentId)}> Ver mais </button>
+          <ul id={marker.equipmentId} className="hide">
+            {getStateHistory(marker.equipmentId).map((item) => (
+              <li key={`li_${item.equipmentStateId}_${item.date}`}>
+                {getStateName(item.equipmentStateId)} | {new Date(item.date).toDateString()}
+              </li>
+            ))}
+          </ul>
+
+        </Popup>
+      </Marker>
+    )
+  });
 
   return (
 
@@ -41,27 +59,23 @@ function MapComponent() {
   )
 };
 
-function createLngLat(lon: number, lat: number): LatLngExpression {
-  return { lng: lon, lat: lat }
-}
 
 function equipmentName(id: string) {
-  var index = eqData.filter(x => x.id === id);  
-  var name = index[0].name;
-  return name;
+  var index = eqData.filter(x => x.id === id);
+  return index[0].name;
 }
 
 function equipmentModelId(id: string) {
   var modelo;
-  var index = eqData.filter(x => x.id === id);  
+  var index = eqData.filter(x => x.id === id);
   var modelId = index[0].equipmentModelId;
-  if (modelId == "a4b0c114-acd8-4151-9449-7d12ab9bf40f"){
-   modelo  = 'Harvester';
-  }else if (modelId == "9c3d009e-0d42-4a6e-9036-193e9bca3199" ){
+  if (modelId === "a4b0c114-acd8-4151-9449-7d12ab9bf40f") {
+    modelo = 'Harvester';
+  } else if (modelId === "9c3d009e-0d42-4a6e-9036-193e9bca3199") {
     modelo = 'Garra Traçadora';
-  }else if (modelId == "a3540227-2f0e-4362-9517-92f41dabbfdf" ){
+  } else if (modelId === "a3540227-2f0e-4362-9517-92f41dabbfdf") {
     modelo = 'Caminhão de Carga';
-  }else{
+  } else {
     modelo = 'desconhecido';
   }
   return modelo;
@@ -70,21 +84,38 @@ function equipmentModelId(id: string) {
 
 
 function equipmentState(id: string) {
-  var index = stateHistoryData.find((x) => x.equipmentId === id); 
-  var state = index?.states[index?.states.length-1].equipmentStateId; 
-  var atual;
-  if (state == "0808344c-454b-4c36-89e8-d7687e692d57"){
-    atual = "operando";
-  }else if (state == "baff9783-84e8-4e01-874b-6fd743b875ad"){
-    atual = "parado";
-  }else if (state == "03b2d446-e3ba-4c82-8dc2-a5611fea6e1f"){
-    atual = "Manutenção";
-  }else {
-    atual = "desconhecido";
+  var equipmentStates = stateHistoryData.filter((x) => x.equipmentId === id)[0];
+  equipmentStates.states = equipmentStates.states.sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime()))
+  var stateId = equipmentStates.states[0].equipmentStateId;
+
+  return stateData.filter((state) => state.id === stateId)[0].name;
+}
+
+function getStateHistory(id: string) {
+  var equipmentStates = stateHistoryData.filter((x) => x.equipmentId === id)[0];
+  return equipmentStates.states.sort((a, b) => (new Date(b.date).getTime() - new Date(a.date).getTime()))
+}
+
+function getStateName(id: string) {
+  return stateData.filter((state) => state.id === id)[0].name;
+}
+
+function showHistory(id: string) {
+
+  let el = document.getElementById(id);
+
+
+  if (el?.classList.contains("hide")) {
+    el.classList.remove('hide');
+    el.classList.add('show');
+    console.log("manteve");
+    return undefined;
+    
   }
-   
-  
-  return atual;
+    el?.classList.remove('show');
+    el?.classList.add('hide');
+    console.log("mudou");
+  return undefined;
 }
 
 
