f9223a905ab9c17a74a9433f1559a15d8dbc5d37
Junior Dering <juniordering@hotmail.com>
2022-06-22 14:42:56 -0400

    criando tabela de estados

---
+++ src/Components/TabelState/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const TabelState: React.FC = () => {
+  return (
+    <p>Testando</p>
+  );
+}
+
+export default TabelState;
