aa428a8376e7577a19048be8713a4f55d889ef8c
Junior Dering <juniordering@hotmail.com>
2022-06-22 14:41:43 -0400

    alterando posicionamento central e estilo

--- src/Components/Map/index.tsx
+++ src/Components/Map/index.tsx
@@ -1,15 +1,20 @@
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import './style.css'
+import { LatLngExpression } from 'leaflet'
+import React from 'react'
 
-function Map() {
-  const position = [51.505, -0.09]
+function Map({children}:{children:React.ReactNode}) {
+
+  const position = [-19.300731, -46.107543] as LatLngExpression
+  
   return (
-    <MapContainer center={[-19.151801, -46.007759]} zoom={11} scrollWheelZoom={true}>
+    <MapContainer center={position} zoom={11} scrollWheelZoom={true}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {children}
     </MapContainer>
 
   )
