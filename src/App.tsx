bc494bf2249839c339c38025d9ba6bb36fe6b0c3
Junior Dering <juniordering@hotmail.com>
2022-06-22 09:31:17 -0400

    iniciando vite

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react'
+import Map from './Components/Map'
+import NavMenu from './Components/NavMenu'
+
+
+function App() {
+
+  return (
+    <Fragment>
+      <NavMenu/>
+      <Map/>
+    </Fragment>
+
+  )
+}
+
+export default App
