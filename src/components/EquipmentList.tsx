5d848b5714c63cf37e4c835c321b9ea8ef2dc335
lucas silva <luqsilva99@gmail.com>
2022-06-27 03:58:32 -0300

    feat: search feature

--- src/components/EquipmentList.tsx
+++ src/components/EquipmentList.tsx
@@ -1,16 +1,35 @@
+import { MagnifyingGlass } from 'phosphor-react';
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import equipments from '../../data/equipment.json';
 
 function EquipmentList() {
     const { equipmentId } = useParams();
+    const [ searchIsVisible, setSearchIsVisible ] = useState(false);
+    const [ searchInput, setSearchInput ] = useState("");
 
     return (
-      <nav>
-        <span className="flex justify-center mt-6 mb-3 text-xl font-bold">Equipamentos</span>
+      <nav className="flex flex-col flex-1">
+        <div className="flex justify-between mx-5 my-3">
+          <span className="text-xl font-bold ">Equipamentos</span>
 
-        <ul className="flex flex-col overflow-x-hidden">
-          {equipments.map(equipment => {
-            const classes = equipment.id == equipmentId ? "flex-1 flex items-center justify-center min-h-[30px] text-blue-400 font-bold scale-110" : "flex-1 flex items-center justify-center min-h-[30px] transition hover:scale-110"
+          <div className="flex items-center cursor-pointer">
+            <MagnifyingGlass weight="bold" onClick={() => {
+              setSearchInput("");
+              searchIsVisible ? setSearchIsVisible(false) : setSearchIsVisible(true)}}
+            />
+          </div>
+        </div>
+
+        <div style={searchIsVisible ? { display: "flex" } : { display: "none" }}>
+          <input placeholder="Digite sua pesquisa" value={searchInput} onChange={(input) => {setSearchInput(input.target.value.toLowerCase())}} className="flex-1 px-4 py-2 mb-2 text-sm text-gray-400 bg-gray-800 outline-none focus-visible:outline-blue-500 focus-visible:outline-2 focus-visible:outline-offset-0" type="search" name="Pesquise um equipamento" />
+        </div>
+
+        <ul className="flex flex-col flex-1 overflow-x-hidden">
+          {equipments.filter((obj) => {
+            return searchInput ? obj.name.toLowerCase().indexOf(searchInput) != -1 : true;
+          }).map(equipment => {
+            const classes = equipment.id == equipmentId ? "flex items-center justify-center min-h-[30px] p-1 text-blue-400 font-bold scale-110" : "p-1 flex items-center justify-center min-h-[30px] transition hover:scale-110"
 
             return <li key={equipment.id} className={classes}>
               <Link className="flex-1 text-center" to={"/" + equipment.id}>{equipment.name}</Link>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import EquipmentList from "./EquipmentList";
 
 function Header() {
 	return (
-		<header className="min-h-screen min-w-[200px] overflow-auto border-r border-gray-500">
+		<header className="flex flex-col min-h-screen min-w-[200px] max-w-[280px] overflow-auto border-r border-gray-500">
 			<a href="#">
-				<img className="px-10 bg-gray-400 bg-center bg-no-repeat bg-cover bg-blend-multiply w-60 py-14 bg-backgroundLogo" src={logo} alt="Aiko" />
+				<img className="flex-1 px-10 bg-gray-400 bg-center bg-no-repeat bg-cover bg-blend-multiply py-14 bg-backgroundLogo" src={logo} alt="Aiko" />
 			</a>
 
 			<EquipmentList />
