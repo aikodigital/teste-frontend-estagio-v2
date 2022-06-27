cde7e09875a02c2c697650b87fc0e3fe00bb7d1e
Laysa Diniz <95299576+santosdlaysa@users.noreply.github.com>
2022-06-27 10:25:16 -0300

    teste

--- my-app/src/components/Equipaments/Equipaments.js
+++ my-app/src/components/Equipaments/Equipaments.js
@@ -1,3 +1,23 @@
-function Equipaments() {
-  return '../src/data/equipment.json';
+import react from "react";
+import props from "../../data/equipment.json"
+import { Root } from 'react-dom/server';
+
+function Name(props) {
+  return <imput type="Maquina" id={props.Name} />;
+
 }
+
+function App () {
+  return (
+    <div>
+      <Name Name="Maquina" />
+      </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+export default Name;
